dc249f4fb2892317f8c805e236d1290918894e15
Dimo Mitev <dimosoftuni@gmail.com>
2025-02-06 21:47:15 +0200

    some change

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file
+// .mocharc.js - Mocha config file Some change
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
